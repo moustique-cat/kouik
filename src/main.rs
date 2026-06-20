df7f282e02489c1dfafd9dd4272a1bdcb0b2bcde
Moustique <112151515+moustique-cat@users.noreply.github.com>
2026-06-19 21:18:57 -0400

    Add slot to the box

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-struct App {}
+use winit::window::Window;
+
+struct App {
+    window: Option<Window>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let app = App { window: None };
+    
 }
